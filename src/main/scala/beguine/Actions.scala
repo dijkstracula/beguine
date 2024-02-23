b431583a08fdadf63b711025e3b95738cc6a9890
Nathan Taylor <ntaylor@cs.utexas.edu>
2024-02-23 15:38:21 -0600

    Actions are better off returning a Witness

--- src/main/scala/beguine/Actions.scala
+++ src/main/scala/beguine/Actions.scala
@@ -1,16 +1,23 @@
 package beguine.actions
 
-class Action0[Z](val name: String, val f: () => Z) extends (() => Z):
 
-  override def apply(): Z = f()
+
+object Witness {
+  case class Call0[Z](f: Action0[Z], ret: Z)
+  case class Call1[A, Z](f: Action1[A, Z], a: A, ret: Z)
+}
+
+class Action0[Z](val name: String, val f: () => Z) extends (() => Witness.Call0[Z]):
+
+  override def apply() = Witness.Call0(this, f())
 
   def tee[Y](g: () => Y): Action0[(Z, Y)] = Action0(name, () => (f(), g()))
 
 
-class Action1[A, Z](agen: => A)(val name: String, val f: A => Z) extends (() => (A, Z)):
-  override def apply(): (A, Z) = {
+class Action1[A, Z](agen: => A)(val name: String, val f: A => Z) extends (() => Witness.Call1[A,Z]):
+  override def apply(): Witness.Call1[A, Z] = {
     val a = agen
-    (a, f(a))
+    Witness.Call1(this, a, f(a))
   }
 
   def tee[Y](g: A => Y): Action1[A, (Z, Y)] = Action1(agen)(name, a => (f(a), g(a)))
--- src/test/scala/Actions.scala
+++ src/test/scala/Actions.scala
@@ -1,6 +1,5 @@
 
-import beguine.actions.Action0
-import beguine.actions.Action1
+import beguine.actions.{Action0, Action1, Witness}
 import beguine.runtime.RandomArbitrary
 import org.scalatest.BeforeAndAfter
 import org.scalatest.funspec.AnyFunSpec
@@ -13,20 +12,24 @@ class ActionTest extends AnyFunSpec with BeforeAndAfter:
 
   describe("Action0"):
     it("can be called"):
-      val a = Action0("f", () => 99)
-      assert(a() == 99)
+      val action = Action0("f", () => 99)
+      val Witness.Call0(f, z) = action()
+      assert(f == action)
+      assert(z == 99)
 
   describe("Action1"):
     it("can be called with a constant generator"):
       val action = Action1[Int, Int](99)("f", n => n+1)
-      val (a, fa) = action()
+      val Witness.Call1(f, a, z) = action()
+      assert(f == action)
       assert(a == 99)
-      assert(fa == 100)
+      assert(z == 100)
 
     it("can be called with a generator explicitly from an Arbitrary"):
       val action = Action1[Int, Int](r.numeric(Some(0), Some(99)))("f", n => n+1)
       for (_ <- 0 to 100) do
-        val (a, fa) = action()
+        val Witness.Call1(f, a, z) = action()
+        assert(f == action)
         assert(0 <= a && a <= 99)
-        assert(1 <= fa && fa <= 100)
+        assert(1 <= z && z <= 100)
 
