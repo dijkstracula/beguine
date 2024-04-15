8bed92bfe9fc7809a6d151248d9d161e7e28bd5a
Nathan Taylor <ntaylor@cs.utexas.edu>
2024-04-15 16:43:52 -0500

    update, ugh

--- src/test/scala/melina/Store.scala
+++ src/test/scala/melina/Store.scala
@@ -1,10 +1,10 @@
 package melina
 
 import accord.api
-import accord.api.{Data, DataStore, Key}
+import accord.api.{Data, DataStore, Key, Write}
 import accord.api.DataStore.FetchResult
 import accord.local.{Node, SafeCommandStore}
-import accord.primitives.{Keys, Ranges, Seekable, Seekables, SyncPoint, Timestamp}
+import accord.primitives.{Keys, PartialTxn, Ranges, Seekable, Seekables, SyncPoint, Timestamp, Writes}
 import accord.utils.Timestamped
 import accord.utils.async.{AsyncChain, AsyncChains, AsyncResult, AsyncResults}
 import melina.Exceptions.NonMelinaEntity
@@ -14,19 +14,19 @@ import scala.collection.mutable
 
 object Store {
   class Data extends accord.api.Data {
-    private val data: mutable.Map[Key, Timestamped[Integer]] = mutable.Map.empty
+    val values: mutable.Map[Seekable, Integer] = mutable.Map.empty
 
-    def this(k: Key, v: Option[Timestamped[Integer]]) = {
+    def this(k: Key, v: Option[Integer]) = {
       this()
       v match {
         case None => ()
-        case Some(v) => data.addOne(k, v)
+        case Some(v) => values.addOne(k, v)
       }
     }
 
     override def merge(other: api.Data): api.Data = {
       other match {
-         case d: Data => data.addAll(d.data)
+         case d: Data => values.addAll(d.values)
          case d => throw NonMelinaEntity(d, this.getClass)
        }
       this
@@ -40,24 +40,58 @@ object Store {
       store match {
         case ms: Store => {
           val k = key.asInstanceOf[Key]
-          val v = ms.values.get(k)
+          val v = ms.values.get(k).map(_.data)
           AsyncChains.success(new Data(k, v))
         }
         case s => throw NonMelinaEntity(s, Store.getClass)
       }
     }
 
-    override def slice(ranges: Ranges): api.Read = new Read(keys.slice(ranges))
+    override def slice(ranges: Ranges): api.Read = new Read(keys slice ranges)
 
     override def merge(other: api.Read): api.Read = other match {
-      case other: Read => new Read(keys.`with`(other.keys))
+      case other: Read => new Read(keys `with` (other.keys))
       case other => throw new NonMelinaEntity(other, this.getClass)
     }
   }
+
+  class Write(data: Data) extends accord.api.Write {
+    override def apply(key: Seekable, safeStore: SafeCommandStore, executeAt: Timestamp, store: DataStore, txn: PartialTxn): AsyncChain[Void] = {
+      store match {
+        case ms: Store =>
+          val ts = new Timestamped[Integer](executeAt, data.values.get(key).getOrElse(null), i => i.toString)
+          ms.values.get(key) match {
+            case None => ms.values.put(key, ts)
+            case Some(existing) =>
+              if (existing.timestamp.compareTo(ts.timestamp) < 0) {
+                ms.values.put(key, ts)
+              }
+          }
+        case other => throw NonMelinaEntity(other, this.getClass)
+      }
+      AsyncChains.success(null)
+    }
+  }
+
+  class Update(keys: Keys) extends accord.api.Update {
+    override def keys = keys
+
+    override def apply(executeAt: Timestamp, data: api.Data): Write = {
+      data match {
+        case d: Data => new Write(d)
+        case other => throw NonMelinaEntity(other, this.getClass)
+      }
+    }
+
+    override def slice(ranges: Ranges): api.Update = new Update(keys slice ranges)
+
+    override def merge(other: api.Update): api.Update = new Update(keys `with` other.keys().asInstanceOf[Keys])
+  }
+
 }
 
 class Store extends DataStore {
-  private val values: mutable.Map[Key, Timestamped[Integer]] = mutable.Map.empty
+  private val values: mutable.Map[Seekable, Timestamped[Integer]] = mutable.Map.empty
 
   class FixMeLaterSuccessfulFetch(ranges: Ranges) extends AsyncResults.SettableResult[Ranges] with FetchResult {
     // XXX: This is fine for the moment.  However, we are going to need
